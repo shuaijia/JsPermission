2adb945beed0cacd9acab0a8ecf515405bc490c8
shuajia <819418850@qq.com>
2017-12-06 17:04:04 +0800

    jia:

--- jspermission/src/main/java/com/jia/jspermission/utils/JsPermissionUtils.java
+++ jspermission/src/main/java/com/jia/jspermission/utils/JsPermissionUtils.java
@@ -49,6 +49,7 @@ public class JsPermissionUtils {
 
     /**
      * 权限请求方法
+     *
      * @param activity
      * @param code
      * @param permissions
@@ -59,24 +60,25 @@ public class JsPermissionUtils {
 
     /**
      * 判断是否需要动态申请权限
+     *
      * @return
      */
-    public static boolean needRequestPermission(){
+    public static boolean needRequestPermission() {
         return Build.VERSION.SDK_INT >= 23;
     }
 
     /**
      * 跳转到权限设置界面
      */
-    public static void getAppDetailSettingIntent(Context context){
+    public static void getAppDetailSettingIntent(Context context) {
         Intent intent = new Intent();
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        if(Build.VERSION.SDK_INT >= 9){
+        if (Build.VERSION.SDK_INT >= 9) {
             intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
             intent.setData(Uri.fromParts("package", context.getPackageName(), null));
-        } else if(Build.VERSION.SDK_INT <= 8){
+        } else if (Build.VERSION.SDK_INT <= 8) {
             intent.setAction(Intent.ACTION_VIEW);
-            intent.setClassName("com.android.settings","com.android.settings.InstalledAppDetails");
+            intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
             intent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
         }
         context.startActivity(intent);
